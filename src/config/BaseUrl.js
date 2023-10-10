81adac57a383ef7733252c4bbf4b2a198414271b
Abhinandan Jhamb <77092975+iam5abhi@users.noreply.github.com>
2023-10-10 17:17:05 +0530

    Update BaseUrl.js
    Update BaseUrl.js

--- src/config/BaseUrl.js
+++ src/config/BaseUrl.js
@@ -1,2 +1,2 @@
 // export default  {url:"http://localhost:7600"}
-export default  {url:"https://restaurant--management-backend.herokuapp.com"}
+export default  {url:"https://payomon-backend.onrender.com"}
